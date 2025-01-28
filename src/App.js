8e541c19f385b47d91f3548b021d87d623bfcf0a
GeyWhenHornie <adarshshukla221@outlook.com>
2025-01-28 17:23:27 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ChatBot from './components/ChatBot';
+
+function App() {
+  return (
+    <div>
+      <h1>Chatbot App</h1>
+      <ChatBot />
+    </div>
+  );
+}
+
+export default App;
